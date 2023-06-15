28190a8ab7ea5b8223ad02b7605fdce0ca6d7fdf
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2023-06-15 15:01:31 +0200

    Add railway app

--- railway/views.py
+++ railway/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Page not Found</h1>")
+
