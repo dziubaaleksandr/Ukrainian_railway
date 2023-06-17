b62b8cc4b4d52f3230dc09d78cfec657307ed96c
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2023-06-17 15:40:56 +0200

    Add superuser, add str to models, register models in admin.py

--- railway/admin.py
+++ railway/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TrainsAdmin(admin.ModelAdmin):
+    list_display = ('status', 'from_city', 'departure_date', 'arrival_date', )
+    list_display_links = ('status',)
+    search_fields = ('status', 'from_city', 'departure_date', 'arrival_date', )
+    list_editable = ('from_city', 'departure_date', 'arrival_date', )
+
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('number', 'train')
+    list_display_links = ('number', 'train')
+    search_fields = ('number', 'train')
+    
+class SeatsAdmin(admin.ModelAdmin):
+    list_display = ('car', 'number', 'status')
+    list_display_links = ('car', 'number', 'status')
+    search_fields = ('car', 'number', 'status')
+
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = ('tickets', )
+    list_display_links = ('tickets', )
+    search_fields = ('tickets', )
+
+
+admin.site.register(Trains, TrainsAdmin)
+admin.site.register(Cars, CarsAdmin)
+admin.site.register(Seats, SeatsAdmin)
+admin.site.register(Clients, ClientsAdmin)
--- railway/models.py
+++ railway/models.py
@@ -10,14 +10,21 @@ class Trains(models.Model):
     departure_date = models.DateTimeField(verbose_name= 'departure_date')
     arrival_date = models.DateTimeField(verbose_name= 'arrival_date')
 
+    def __str__(self):
+        return f"{self.from_city} {self.to_city}"
+    
 class Cars(models.Model):
     train = models.ForeignKey('Trains', on_delete=models.SET_NULL, null=True)
     number = models.IntegerField(verbose_name= 'Car number')
+    def __str__(self):
+        return f"{self.train} {self.number}"
 
 class Seats(models.Model):
     car = models.ForeignKey('Cars', on_delete=models.CASCADE, null=True)
     number = models.IntegerField(verbose_name= 'Seat number')
     status = models.BooleanField(verbose_name= 'Seat status')
+    def __str__(self):
+        return f"{self.car} {self.number}"
 
 class Clients(AbstractUser):
     tickets = models.ForeignKey('Seats', on_delete=models.CASCADE, null=True)
