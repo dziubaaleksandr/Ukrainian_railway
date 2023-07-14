f864cfde1a60e55678dd54dabfcf29f0b1e49d8c
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2023-07-14 09:44:23 +0200

    Fix search bug in admin panel

--- railway/admin.py
+++ railway/admin.py
@@ -4,21 +4,20 @@ from .models import *
 class TrainsAdmin(admin.ModelAdmin):
     list_display = ('number', 'status', 'from_city', 'to_city', 'departure_date', 'arrival_date', )
     list_display_links = ('number',)
-    search_fields = ('status', 'from_city', 'departure_date', 'arrival_date', )
-    list_editable = ('status', 'from_city', 'departure_date', 'arrival_date', )
+    search_fields = ('status', 'from_city', 'to_city', 'departure_date', 'arrival_date', )
+    list_editable = ('status', 'from_city', 'to_city', 'departure_date', 'arrival_date', )
     prepopulated_fields = {'slug': ('from_city', 'to_city' )}
+
 class CarsAdmin(admin.ModelAdmin):
     list_display = ('number', 'train')
     list_display_links = ('number', 'train')
-    search_fields = ('number', 'train')
+    search_fields = ('number', 'train__slug')
     
 class SeatsAdmin(admin.ModelAdmin):
     list_display = ('car', 'number', 'user', 'status')
     list_display_links = ('car', 'number',)
     list_editable = ('user', 'status', )
-    search_fields = ('car', 'number', 'status')
-
-
+    search_fields = ('car__number', 'car__train__from_city', 'car__train__to_city', 'number', 'status')
 
 admin.site.register(Trains, TrainsAdmin)
 admin.site.register(Cars, CarsAdmin)
